bd2b3d731efb91af6e1e526002a491c2e55ab808
Hashan Perera <hashan.p@digitustecglobal.com>
2023-07-10 09:07:39 +0530

    second commit by hash

--- edu_app/lib/main.dart
+++ edu_app/lib/main.dart
@@ -1,22 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 
-void main() {
-  runApp(const MyApp());
+import 'screens/welcome_screen.dart';
+
+void main(){
+  runApp(
+    DevicePreview(
+      enabled: !kReleaseMode,
+      builder: (context) => MyApp(), // Wrap your app
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+ 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
+      debugShowCheckedModeBanner: false,
+      title: 'Education App',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        scaffoldBackgroundColor: Colors.white
       ),
-      ///home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: WelcomeScreen()
     );
   }
 }
---
+++ edu_app/lib/screens/home_screen.dart
@@ -0,0 +1,265 @@
+import 'package:edu_app/screens/course_screen.dart';
+import 'package:flutter/material.dart';
+
+// ignore: must_be_immutable
+class HomePage extends StatelessWidget {
+  //Create Statuc Data in  a List
+  List catNames = [
+    "Category",
+    "Classes",
+    "Free Course",
+    "Book Store",
+    "Live Course",
+    "LeaderBoard",
+  ];
+
+  List<Color> catColors = [
+    Color(0xFFFFCFC2),
+    Color(0xFF6FE08D),
+    Color(0xFF61BDFD),
+    Color(0xFFCB84FB),
+    Color(0xFF78E667),
+    Color(0xFFFC7C7F),
+  ];
+
+  List<Icon> catIcons = [
+    Icon(
+      Icons.category,
+      color: Colors.white,
+      size: 30,
+    ),
+    Icon(
+      Icons.video_library,
+      color: Colors.white,
+      size: 30,
+    ),
+    Icon(
+      Icons.assignment,
+      color: Colors.white,
+      size: 30,
+    ),
+    Icon(
+      Icons.store,
+      color: Colors.white,
+      size: 30,
+    ),
+    Icon(
+      Icons.play_circle_fill,
+      color: Colors.white,
+      size: 30,
+    ),
+    Icon(
+      Icons.emoji_events,
+      color: Colors.white,
+      size: 30,
+    ),
+  ];
+
+  List catImages = ['Flutter', 'React Native', 'Python', 'C#'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: [
+          Container(
+            padding: EdgeInsets.only(top: 15, left: 15, right: 15, bottom: 10),
+            decoration: BoxDecoration(
+                color: Color(0xFF674AEF),
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(20),
+                  bottomRight: Radius.circular(20),
+                )),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Icon(
+                      Icons.dashboard,
+                      size: 30,
+                      color: Colors.white,
+                    ),
+                    Icon(
+                      Icons.notifications,
+                      size: 30,
+                      color: Colors.white,
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 3, bottom: 15),
+                  child: Text(
+                    "Hi Programmers",
+                    style: TextStyle(
+                        fontSize: 25,
+                        fontWeight: FontWeight.w600,
+                        letterSpacing: 1,
+                        wordSpacing: 2,
+                        color: Colors.white),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 5, bottom: 20),
+                  padding: EdgeInsets.only(left: 5),
+                  width: MediaQuery.of(context).size.width,
+                  height: 55,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: "Search...",
+                        hintStyle:
+                            TextStyle(color: Colors.black.withOpacity(0.5)),
+                        prefixIcon: Icon(
+                          Icons.search,
+                          size: 25,
+                        )),
+                  ),
+                )
+              ],
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 20, left: 15, right: 15),
+            child: Column(children: [
+              GridView.builder(
+                itemCount: catNames.length,
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  childAspectRatio: 1.1,
+                ),
+                itemBuilder: (context, index) {
+                  return Column(
+                    children: [
+                      Container(
+                        height: 60,
+                        width: 60,
+                        decoration: BoxDecoration(
+                          color: catColors[index],
+                          shape: BoxShape.circle,
+                        ),
+                        child: Center(child: catIcons[index]),
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        catNames[index],
+                        style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.black.withOpacity(0.6)),
+                      )
+                    ],
+                  );
+                },
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("Courses",
+                      style: TextStyle(
+                        fontSize: 23,
+                        fontWeight: FontWeight.w600,
+                      )),
+                  Text("See All",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w600,
+                        color: Color(0xFF674AEF),
+                      )),
+                ],
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              GridView.builder(
+                itemCount: catImages.length,
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  childAspectRatio:
+                      (MediaQuery.of(context).size.height - 50 - 25) /
+                          (4 * 240),
+                  mainAxisSpacing: 10,
+                  crossAxisSpacing: 10,
+                ),
+                itemBuilder: (context, index) {
+                  return InkWell(
+                    onTap: () {
+                      Navigator.push(context, MaterialPageRoute(
+                        builder: (context) => CourseScreen(catImages[index]),
+                      ));
+                    },
+                    child: Container(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: Color(0xFFF5F3FF)),
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.all(10),
+                            child: Image.asset('assets/${catImages[index]}.png',
+                                width: 100,
+                                height: 100,
+                                ),
+                          ),
+                          SizedBox(height: 10,),
+                          Text(
+                            catImages[index],
+                            style: TextStyle(
+                              fontSize: 22,
+                              fontWeight: FontWeight.w600,
+                              color: Colors.black.withOpacity(0.6),
+                            ),
+                          ),
+
+                          SizedBox(height: 10,),
+                          Text(
+                            "55 Videos",
+                            style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.w500,
+                              color: Colors.black.withOpacity(0.5)
+                            ),
+                          )
+
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ]),
+          ),
+        ],
+      ),
+    
+      bottomNavigationBar: BottomNavigationBar(
+        showUnselectedLabels: true,
+        iconSize: 32,
+        selectedItemColor: Color(0xFF674AEF),
+        selectedFontSize: 18,
+        unselectedItemColor: Colors.grey,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.assignment), label: 'Course'),
+          BottomNavigationBarItem(icon: Icon(Icons.favorite), label: 'Wishlist'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Account'),
+      ],
+      ),
+
+    );
+  }
+}
